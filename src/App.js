0f3e0439ee2826e4d206c16e31510e013b030d39
sahil keshav <sahilKeshav1997@gmail.com>
2021-03-01 21:51:45 +0530

    deploying to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import imgData from "./data";
+import { BiLeftArrowCircle, BiRightArrowCircle } from "react-icons/bi";
 
-function App() {
+const App = () => {
+  const [current, setCurrent] = useState(0);
+  const length = imgData.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setCurrent(current + 1);
+      if (current === length - 1) {
+        setCurrent(0);
+      }
+    }, 3000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [current]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h2>slider</h2>
+        <div className="underline"></div>
       </header>
-    </div>
+      <section>
+        <BiLeftArrowCircle className="arrowLeft" onClick={prevSlide} />
+        <BiRightArrowCircle className="arrowRight" onClick={nextSlide} />
+        {imgData.map((image, index) => {
+          const { id, img, name } = image;
+          // console.log(index);
+          // console.log(current);
+          return (
+            <div
+              key={id}
+              className={index === current ? "slide active" : "slide"} //adding class based on condition
+            >
+              {index === current && <img src={img} alt={name} />}
+              {/* short circuit evaluation*/}
+            </div>
+          );
+        })}
+      </section>
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const imgData = [
+  {
+    id: 1,
+    img:
+      "https://images.unsplash.com/photo-1550408483-bafa35b0a433?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1037&q=80",
+    name: "nature",
+  },
+  {
+    id: 2,
+    img:
+      "https://images.unsplash.com/photo-1614597388646-fea96377eb91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    name: "nature",
+  },
+  {
+    id: 3,
+    img:
+      "https://images.unsplash.com/photo-1614478917652-15e5b8ffe2a3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80",
+    name: "nature",
+  },
+  {
+    id: 4,
+    img:
+      "https://images.unsplash.com/photo-1614368558175-ad4a95a6d8d6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80",
+    name: "nature",
+  },
+  {
+    id: 5,
+    img:
+      "https://images.unsplash.com/photo-1614368558359-9ab23db1bab3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80",
+    name: "nature",
+  },
+];
+
+export default imgData;
